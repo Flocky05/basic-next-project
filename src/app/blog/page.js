91783a94a79c97cfd8fb7c19024f8e78b1f335c6
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-12 23:56:07 +0600

    added front page

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -3,7 +3,20 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <h2> This is blog page</h2>
+      <div className="card w-96 bg-base-100 shadow-xl image-full">
+        <figure>
+          <img src="" alt="Shoes" />
+        </figure>
+        <div className="">
+          <h2 className="">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="">
+            <button className="px-4 py-1 bg-blue-300 rounded-md">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -7,10 +7,11 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda cumque
-      soluta consectetur, atque beatae rem dignissimos quia libero, vero,
-      voluptate facilis odio sint accusantium itaque dolores. Dolores aliquid
-      quos cupiditate.
+      <img
+        src="https://img.freepik.com/free-photo/abstract-smoke-wallpaper-background-desktop_53876-128257.jpg?w=996&t=st=1676223251~exp=1676223851~hmac=3e03c45eae61b6209c0d99c78e1824534d03ab4337bd1b565c798b821b432b81"
+        alt=""
+        className="w-full h-full object-none"
+      />
     </main>
   );
 }
